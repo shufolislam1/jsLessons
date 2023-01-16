49088ef3d820eb7afcad9c70477f61da74c51168
shufol islam <shufolislam1255@gmail.com>
2023-01-16 17:12:11 +0600

    completed var

---
+++ JsVariable.js
@@ -0,0 +1,20 @@
+//  x = 5;
+//  y = 6;
+//  z = x + y;
+
+// console.log(z);
+
+// var carName;  
+// // undefined
+// carName = 45;
+
+// var carName = "Volvo";
+// var carName;
+
+// console.log(carName);
+
+let x = "5" + 2 + 3;
+// '52' string
+// '523' string
+
+console.log(  typeof x);
---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * var, const , const
+ */
+
+//  x = 5;
+//  y = 40;
+//  z = 45;
+//  res = x+y+z;
+// console.log(res);
+
+let x = 10;
+// Here x is 10
+console.log(x);
+
+{
+let x = 2;
+// Here x is 2
+console.log(x);
+
+}
+console.log(x);
+
+// Here x is 2
