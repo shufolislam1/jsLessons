bba3b0c75c252b2ac1f9a710dd944420ad0a9ff0
shufol islam <shufolislam1255@gmail.com>
2023-01-23 19:55:06 +0600

    let and block scope done

---
+++ scope.js
@@ -0,0 +1,25 @@
+// let x = 20;
+// // 20
+// {
+//     let x = 2;
+//     // 2
+//     console.log(x);
+// }
+// console.log(x);
+//   // x can NOT be used here
+
+// var x = "John Doe";
+
+// var x = 0;
+// console.log(x);
+
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+console.log(x);
+}
+
+// Here x is 2
